6e4041512722eb84dc846ff722d5e4b72ec07490
JP Luttrell <hyzenthlay12@yahoo.com>
2019-11-18 20:36:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
   if (age > 12 && age < 20) {
     return "You are a teenager!"
   } else if (age < 13) {
-    return "You are kid"
+    return "You are a kid"
   } else {
     return "You are a grownup"
   }
